05b3012542747d6389a3e4532e65e2c5e219b728
Haeori <seungyeon.dev@gmail.com>
2026-02-25 17:46:08 +0900

    Initial commit from Create Next App

---
+++ src/components/movie/movie-detail.tsx
@@ -0,0 +1,7 @@
+
+export const MovieContents = () => {
+    return <>
+    <h1>ONEBITE CINEMA</h1>
+        <img src="" alt=""/>
+    </>
+}
---
+++ src/pages/movie/[id].tsx
@@ -0,0 +1,11 @@
+
+// 영화 상세 페이지
+import {MovieDetail} from "@/components/movie/movie-detail";
+
+const Movie = () => {
+    <>
+        <MovieDetail />
+</>
+}
+
+export default Movie;
