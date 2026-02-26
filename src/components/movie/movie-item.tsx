6049868d77474b6e75a8546345651afed0c8969a
Haeori <seungyeon.dev@gmail.com>
2026-02-26 16:06:36 +0900

    feature: Cinema Movie Page 작업
    - Cinema Movie Page 작업

--- src/components/movie/movie-item.tsx
+++ src/components/movie/movie-item.tsx
@@ -1,10 +1,14 @@
 import { MovieInfo } from '@/types/movie-types';
-import style from '@/styles/movie.module.css';
+import style from '@/styles/movie-item.module.css';
+import { useRouter } from 'next/router';
 
 type MovieItemProps = {
   movie: MovieInfo;
 };
 
 export const MovieItem = ({ movie }: MovieItemProps) => {
-  return <img className={style.moviePoster} src={movie.posterImgUrl} alt={movie.title} onClick={() => {}} />;
+  const router = useRouter();
+
+  // TODO 추후 실데이터 연결
+  return <img className={style.moviePoster} src={movie.posterImgUrl} alt={movie.title} onClick={() => router.push(`/movie/${movie.id}`)} />;
 };
