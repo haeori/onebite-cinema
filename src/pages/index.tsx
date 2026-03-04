1e06bbae473e2d872194b74009963812149c081f
Haeori <seungyeon.dev@gmail.com>
2026-03-04 13:32:38 +0900

    feat: Cinema Home Page에 SSR 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,25 @@
 import { ReactNode } from 'react';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 
 import { SearchbarLayout } from '@/components/layouts/searchbar-layout';
 
 import { CinemaHome } from '@/components/cinema-home';
+import { fetchMovieList, fetchRandomMovies } from '@/pages/api/movie';
 
-export default function HomePage() {
-  return <CinemaHome />;
+export const getServerSideProps: GetServerSideProps = async () => {
+  const movies = await fetchMovieList();
+  const recommendMovies = await fetchRandomMovies();
+
+  return {
+    props: {
+      movies,
+      recommendMovies
+    },
+  }
+};
+
+export default function HomePage({movies, recommendMovies}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return <CinemaHome movies={movies} recommendMovies={recommendMovies} />;
 }
 
 HomePage.getLayout = (page: ReactNode) => {
