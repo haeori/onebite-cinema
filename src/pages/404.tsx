60fc0c4f81e71316bb29b8b96b817aee40279dbe
Haeori <seungyeon.dev@gmail.com>
2026-03-04 17:08:36 +0900

    style: 404 스타일 개선

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,19 @@
+import Link from 'next/link';
 import { GlobalLayout } from '@/components/layouts/global-layout';
+import style from '@/styles/notfound.module.css';
 
 const NotFound = () => (
   <GlobalLayout>
-    <h1>존재하지 않는 페이지입니다.</h1>
+    <div className={style.container}>
+      <div className={style.icon}>🎬</div>
+      <h1 className={style.title}>페이지를 찾을 수 없습니다</h1>
+      <p className={style.description}>
+        요청하신 페이지가 존재하지 않거나 삭제되었습니다.
+      </p>
+      <Link href="/" className={style.button}>
+        홈으로 돌아가기
+      </Link>
+    </div>
   </GlobalLayout>
 );
 
