04c3cb66214f779962cb0664d83ee7bb4f97e12d
Haeori <seungyeon.dev@gmail.com>
2026-03-05 10:24:16 +0900

    feat: 페이지 전환 시 Progress Bar 추가
    - nextjs-progressbar 라이브러리를 사용해 로딩 표시 구현

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import type { AppProps } from 'next/app';
+import NextNProgress from 'nextjs-progressbar';
 
 import '@/styles/globals.css';
 
@@ -14,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps & { Component: La
   return (
     <>
       <GlobalLayout>{getLayout(<Component {...pageProps} />)}</GlobalLayout>
+      <NextNProgress color="#6b1fa8" height={4} options={{ showSpinner: false }} />
     </>
   );
 }
